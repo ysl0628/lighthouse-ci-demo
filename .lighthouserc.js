4801cfefa92d13ac7d3e302d4e77634b90229696
ReneeLan <reneelan@eztravel.com.tw>
2023-11-02 15:01:13 +0800

    feat: setup emulatedFormFactor for desktop

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -3,7 +3,10 @@ module.exports = {
       assert: {
         collect: {
           url:process.env.CI ? "https://lighthouse-ci-demo.vercel.app/" : "http://localhost:3000",
-          startServerCommand: process.env.CI ? undefined : "npm run dev"
+          startServerCommand: process.env.CI ? undefined : "npm run dev",
+          settings: {
+            emulatedFormFactor: 'desktop',
+          },
           },
         preset: "lighthouse:no-pwa",
         assertions: {
