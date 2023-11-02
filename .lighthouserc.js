d6a9dcb683a87b0f7bea08a7b9c91e383e0add26
ReneeLan <reneelan@eztravel.com.tw>
2023-11-02 13:35:10 +0800

    fix: modify lighthouserc.js

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,8 +1,9 @@
-{
+module.exports = {
     "ci": {
       "assert": {
         "collect": {
-            "staticDistDir": "./out"
+            "url":process.env.NODE_ENV === 'production' ? "https://lighthouse-ci-demo.vercel.app/" : "http://localhost:3000",
+            "startServerCommand": process.env.NODE_ENV === 'production' ? undefined :"npm run dev"
           },
         "preset": "lighthouse:no-pwa",
         "assertions": {
@@ -14,6 +15,9 @@
             "error",
             { "aggregationMethod": "optimistic", "minScore": 0.95 }
           ]
+        },
+        "upload": {
+          "target": 'temporary-public-storage',
         }
       }
     }
